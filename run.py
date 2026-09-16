cdf95f69c3207abe2534afd86e4cab942e49c71d
kijoojootopia <kijoojoo010401@gmail.com>
2026-09-16 20:39:24 +0900

    Initial commit

---
+++ run.py
@@ -0,0 +1,9 @@
+"""VS Code terminal: python run.py"""
+
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=int(os.getenv("PORT", "5000")), debug=False)
